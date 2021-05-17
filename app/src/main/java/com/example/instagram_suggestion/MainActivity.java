3a5cffa34c8adb1b78a93ae80d594134aaf2cc55
Rohitohlyan66 <ohlyanrohit66@gmail.com>
2021-05-17 11:44:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/instagram_suggestion/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.instagram_suggestion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.ObjectAnimator;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.LinearInterpolator;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    ProgressBar bar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        bar = findViewById(R.id.bars);
+
+
+        ObjectAnimator animator = ObjectAnimator.ofInt(bar, "progress", 100);
+        animator.setDuration(4000);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.start();
+
+
+    }
+}
